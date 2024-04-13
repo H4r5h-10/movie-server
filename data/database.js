69294888b8a26b46c958b5d8c6ba564e87f033d7
Har5h <103768218+H4r5h-10@users.noreply.github.com>
2024-04-13 15:35:11 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = ()=>{
-    mongoose.connect("mongodb://localhost:27017",{
+    mongoose.connect("mongodb+srv://H4r5h-10:9TlzDXKb9cAaRUcS@cluster0.wmv3dto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
     dbName: "authentication_app"
 }).then(console.log("Database Connected"));
 }
