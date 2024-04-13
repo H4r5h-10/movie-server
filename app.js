c305e2289a85ddde10a6e097c2605bf98e6a54f2
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-04-13 15:25:05 +0530

    Second commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import connectDB from "./data/database.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 const app = express();
 
@@ -9,9 +10,16 @@ connectDB();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.listen(4000, (req,res)=>{
-  console.log("Server Running at http://localhost:4000")
+  console.log("Server Running")
 })
 
 export default app;
