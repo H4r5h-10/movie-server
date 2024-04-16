0598d07f80a72a9d2893c5136fdbf2f8f6eaabc9
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-04-16 17:51:49 +0530

    Update Bug

--- controllers/features.js
+++ controllers/features.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 const sendCookie = (user,res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, "HUHUHUHUHUHHUH");
   res.status(statusCode).cookie("token", token, {
+      httpOnly: true,
       maxAge: 2*24*60*60*1000,
       sameSite: "none",
-      secure:  true,
     })
     .json({
       success: true,
