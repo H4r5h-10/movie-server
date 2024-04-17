b8bc1b729b9e298815d033cc5b25f84e89e1a819
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-04-17 07:13:30 +0530

    Add origins

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://moviedb-r2ms.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
