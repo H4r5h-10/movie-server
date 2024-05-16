9b2d60cf5121f189f3041b541cff2580e7d97270
Har5h <103768218+H4r5h-10@users.noreply.github.com>
2024-05-17 02:52:31 +0530

    Update features.js

--- controllers/features.js
+++ controllers/features.js
@@ -3,16 +3,11 @@ import jwt from "jsonwebtoken";
 export const sendCookie = (user,res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id },process.env.APP_SECRET_KEY);
 
-  res.status(statusCode).cookie("token", token, {
-    domain: "moviemeter-sigma.vercel.app",
-      maxAge: 2*24*60*60*1000,
-      sameSite: 'none',
-      secure: true,
-    httpOnly: true
-    })
+  res.status(statusCode)
     .json({
       success: true,
       message,
+      token
     });
 };
 export default sendCookie;
